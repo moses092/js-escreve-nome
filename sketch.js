b9e4a4ca6c9cf77bbf687b4c3817d0f68fd807b4
moses092 <moisesjousa@gmail.com>
2024-06-28 07:59:28 -0300

    alterei o fundo do js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
 function setup() {
     createCanvas(400, 400);
+    background("black");
   }
   
   function draw() {
     background(220);
+    stroke("blue");
+   FileList("red");
+
+   if(mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35);
+   }
+
+
+
   }
